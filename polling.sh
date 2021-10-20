86f599cc4dc85efc7069af7c351bd9f0b87c1f94
Kevin M. Gates <kevinmgates@users.noreply.github.com>
2021-10-20 14:18:20 -0500

    Update polling.sh

--- polling.sh
+++ polling.sh
@@ -47,19 +47,44 @@ healthcheck() {
     echo $result
 }
 
-while [[ true ]]; do
-   result=`healthcheck $endpoint` 
+i=0
+while [ $i -le 10 ]
+do
+  #echo "Number:" $i((i++))
+  result=`healthcheck $endpoint` 
    declare status
    if [[ -z $result ]]; then 
       status="N/A"
    else
-      status=${result:9:3}
+      status=${result:7:3}
+      echo $status
+      if [ "$status" == "200" ]; then
+        break
+      fi
    fi 
-   timestamp=$(date "+%Y%m%d-%H%M%S")
-   if [[ -z $hasUrl ]]; then
-     echo "$timestamp | $status "
-   else
-     echo "$timestamp | $status | $endpoint " 
-   fi 
-   sleep $duration
+   i=$((i+1))
+   sleep 60
 done
+
+if [ "$status" == "200" ]; then
+  exit 0
+else
+  exit 1
+fi
+
+# while [[ true ]]; do
+#    result=`healthcheck $endpoint` 
+#    declare status
+#    if [[ -z $result ]]; then 
+#       status="N/A"
+#    else
+#       status=${result:7:3}
+#    fi 
+#    timestamp=$(date "+%Y%m%d-%H%M%S")
+#    if [[ -z $hasUrl ]]; then
+#      echo "$status "
+#    else
+#      echo "$timestamp | $status | $endpoint " 
+#    fi 
+#    sleep $duration
+# done
